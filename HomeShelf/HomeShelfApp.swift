da7ce327fe74e1bfc547cb600e8f55e5067560dd
Mykola Chaikovskyi <nickchaywork@gmail.com>
2023-06-08 12:41:59 +0300

    Initial Commit

---
+++ HomeShelf/HomeShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeShelfApp.swift
+//  HomeShelf
+//
+//  Created by Mykola Chaikovskyi on 08.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct HomeShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
