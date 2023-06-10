17b077ad5e33245b81cc55879b6181f5b107de79
Mykola Chaikovskyi <nickchaywork@gmail.com>
2023-06-10 10:51:20 +0300

    Functional image selector

--- HomeShelf/NewBookView.swift
+++ HomeShelf/NewBookView.swift
@@ -6,10 +6,12 @@
 //
 
 import SwiftUI
+import AppKit
 
 struct NewBookView: View {
     @Binding var addBookSheetActivated: Bool
     @State var titleFieldText: String = ""
+    @State var coverPictureFilename: String = ""
     
     var body: some View {
         VStack {
@@ -30,9 +32,19 @@ struct NewBookView: View {
                         .padding(.bottom)
                     HStack {
                         Text("Cover")
-                            .padding(.bottom)
-                        Rectangle()
-                            .frame(width: 50, height: 75).cornerRadius(9)
+                            .padding(.trailing)
+                        Button(action: imageSelectButtonAction) {
+                            Text("Browse...")
+                        }
+                        if (coverPictureFilename != "") {
+                            Image(nsImage: NSImage.init(byReferencingFile: coverPictureFilename)!)
+                                .resizable()
+                                .frame(width: 50, height: 75).cornerRadius(9)
+                        }
+                        else {
+                            Rectangle()
+                                .frame(width: 50, height: 75).cornerRadius(9)
+                        }
                     }
                     
                 }.padding(.top).frame(width: 270)
@@ -54,6 +66,15 @@ struct NewBookView: View {
     
     func dummyButtonAction() {
         addBookSheetActivated = false
+    }
+    
+    func imageSelectButtonAction() {
+        let panel = NSOpenPanel()
+                panel.allowsMultipleSelection = false
+                panel.canChooseDirectories = false
+                if panel.runModal() == .OK {
+                    self.coverPictureFilename = panel.url?.path ?? "<none>"
+        }
     }
 }
 
